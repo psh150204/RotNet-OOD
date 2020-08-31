900d25a5a0595c4cc3a951d980b13b69a798030f
psh150204 <psh131226@gmail.com>
2020-08-31 06:04:57 +0000

    version 1.0

--- test.py
+++ test.py
@@ -65,7 +65,7 @@ def main():
     # data loader  
     id_test_loader = dataloader(id_testdata, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True)
     ood_test_loader = dataloader(ood_testdata, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=True)
-  
+
     # load model
     num_classes = 10
     model = WideResNet(args.layers, num_classes, args.widen_factor, dropRate=args.droprate)
@@ -73,11 +73,65 @@ def main():
     model = model.cuda()
     model.load_state_dict(torch.load('./models/trained_model_{}.pth'.format(args.method)))
 
-    TODO:
-    # 1. calculate ood score by two methods(MSP, Rot)
+    with torch.no_grad():
+        # 1. calculate ood score by two methods(MSP, Rot)
+        ood_scores = []
+
+        for x_tf_0, x_tf_90, x_tf_180, x_tf_270, _ in id_test_loader:
+            if args.method == 'msp':
+                ood_scores.append(-1 * torch.max(F.softmax(model(x_tf_0.cuda())[0], dim=-1), dim=-1).values)
+            elif args.method == 'rot':
+                ood_scores.append(rot_ood_score(
+                    x_tf_0, x_tf_90, x_tf_180, x_tf_270, model))
+            else :
+                raise ValueError(args.method)
+
+        for x_tf_0, x_tf_90, x_tf_180, x_tf_270, _ in ood_test_loader:
+            if args.method == 'msp':
+                ood_scores.append(-1 * torch.max(F.softmax(model(x_tf_0.cuda())[0], dim=-1), dim=-1).values)
+            else:
+                ood_scores.append(rot_ood_score(
+                    x_tf_0, x_tf_90, x_tf_180, x_tf_270, model))
 
     # 2. calculate AUROC by using ood scores 
+    ood_scores = torch.cat(ood_scores, dim = 0).cpu()
+
+    ground_truth = torch.zeros(len(id_test_loader.dataset))
+    ground_truth = torch.cat([ground_truth, torch.ones(len(ood_test_loader.dataset))], dim = 0)
     
+    print('AUROC of ' + args.ood_dataset + ' with ' + args.method + ' is %.4f'%(roc_auc_score(ground_truth, ood_scores)))
+
+
+def rot_ood_score(x_tf_0, x_tf_90, x_tf_180, x_tf_270, model):
+    batch_size = x_tf_0.size(0)
+    batch_x = torch.cat(
+        [x_tf_0, x_tf_90, x_tf_180, x_tf_270], 0).cuda()
+
+    batch_rot_y = torch.cat((
+        torch.zeros(batch_size),
+        torch.ones(batch_size),
+        2 * torch.ones(batch_size),
+        3 * torch.ones(batch_size)
+        ), 0).long().cuda()
+
+    logits, pen = model(batch_x)
+
+    classification_logits = logits[:batch_size]
+    prob = F.softmax(classification_logits, dim=-1)
+    uniform_dist = torch.zeros_like(prob).fill_(0.1)
+    KLdiv = (prob * (prob/uniform_dist).log()).sum(dim=-1)
+
+    rot_logits = model.rot_head(pen)
+    rot_loss = F.cross_entropy(
+        rot_logits, batch_rot_y, reduction='none')
+    rot_losses = list(rot_loss.split(batch_size))
+    for loss in rot_losses:
+        loss.unsqueeze_(0)
+
+    rot_loss = torch.cat(rot_losses, dim=0).mean(dim=0)
+
+    return -1 * KLdiv + rot_loss
+
 
 if __name__ == "__main__":
     main()
